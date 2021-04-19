d70ea7c55dfd83d38b4936e792d4227c79604599
Birol Uzun <Brl.uzn2017@gmail.com>
2021-04-19 08:06:36 +0300

    first query finished

--- app/src/main/java/com/example/taxi_map/MainActivity.java
+++ app/src/main/java/com/example/taxi_map/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.taxi_map;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,8 +29,9 @@ import java.util.Collections;
 public class MainActivity extends AppCompatActivity {
 
     Button btn_sorgula;
-    TextView textView_veri;
-    ArrayList<Veri> veriler;
+    TextView r1c1,r1c2,r2c1,r2c2,r3c1,r3c2,r4c1,r4c2,r5c1,r5c2;
+    ExtendedFloatingActionButton fab1;
+
 
     FirebaseDatabase database=FirebaseDatabase.getInstance();
     DatabaseReference reference=database.getReference();
@@ -34,39 +40,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        veriler=new ArrayList<>();
+        fab1=findViewById(R.id.fab1);
+        TableLayout table;
 
-        btn_sorgula=findViewById(R.id.btn_sorgula);
-        textView_veri=findViewById(R.id.textView_veri);
+        btn_sorgula=findViewById(R.id.btn_sorgu_bir);
+        table=findViewById(R.id.table);
+        r1c1=findViewById(R.id.r1c1);
+        r1c2=findViewById(R.id.r1c2);
+        r2c1=findViewById(R.id.r2c1);
+        r2c2=findViewById(R.id.r2c2);
+        r3c1=findViewById(R.id.r3c1);
+        r3c2=findViewById(R.id.r3c2);
+        r4c1=findViewById(R.id.r4c1);
+        r4c2=findViewById(R.id.r4c2);
+        r5c1=findViewById(R.id.r5c1);
+        r5c2=findViewById(R.id.r5c2);
 
-        reference.addValueEventListener(new ValueEventListener() {
+        first_query();
+
+        btn_sorgula.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onClick(View v) {
 
-                for(DataSnapshot d:dataSnapshot.getChildren()){
-                    Veri veri=d.getValue(Veri.class);
-                    veriler.add(veri);
-                    textView_veri.setText("Veri Çekme Başarılı");
-
-
-                }
-
-                if (veriler != null) {
-                    first_query();
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                Toast.makeText(MainActivity.this, "Hata İle Karşılaşıldı", Toast.LENGTH_SHORT).show();
-
+                table.setVisibility(View.VISIBLE);
             }
         });
 
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),SecondActivity.class);
+                startActivity(intent);
 
-
+            }
+        });
 
 
 
@@ -75,6 +82,37 @@ public class MainActivity extends AppCompatActivity {
     private void first_query(){
 
 
-        textView_veri.setText(String.valueOf(veriler.get(1).getID()));
+        ArrayList<Veri> max_trip_distance=new ArrayList<>();
+
+        reference.orderByChild("trip_distance").limitToLast(5).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot d:dataSnapshot.getChildren()){
+                    Veri data=d.getValue(Veri.class);
+                    max_trip_distance.add(data);
+
+                }
+                r1c1.setText(max_trip_distance.get(4).getTpep_dropoff_datetime());
+                r1c2.setText(String.valueOf(max_trip_distance.get(4).getTrip_distance()));
+                r2c1.setText(max_trip_distance.get(3).getTpep_dropoff_datetime());
+                r2c2.setText(String.valueOf(max_trip_distance.get(3).getTrip_distance()));
+                r3c1.setText(max_trip_distance.get(2).getTpep_dropoff_datetime());
+                r3c2.setText(String.valueOf(max_trip_distance.get(2).getTrip_distance()));
+                r4c1.setText(max_trip_distance.get(1).getTpep_dropoff_datetime());
+                r4c2.setText(String.valueOf(max_trip_distance.get(1).getTrip_distance()));
+                r5c1.setText(max_trip_distance.get(0).getTpep_dropoff_datetime());
+                r5c2.setText(String.valueOf(max_trip_distance.get(0).getTrip_distance()));
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
     }
 }
